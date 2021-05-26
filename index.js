673ef2ce58c749bcbbfa87ad2999cadf269df8bc
MorganBare <morganjbare@gmail.com>
2021-05-26 07:37:47 +0200

    Added delete button

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
+let germanWords = []
+let storedGermanWords = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const delbtn = document.getElementById("del-btn")
+const germanWordsFromLocalStorage = JSON.parse( localStorage.getItem("germanWords"))
 
-let germanWords = []
+if (germanWordsFromLocalStorage) {
+    germanWords = germanWordsFromLocalStorage
+    renderWords(germanWords)
+}
 
-inputBtn.addEventListener("click", function(){
-    germanWords.push(inputEl.value)
-    inputEl.value = ""
-    renderWords()
-})
-
-function renderWords() {
+function renderWords(words) {
     let listItems = ""
-for (let i = 0; i < germanWords.length; i++){
-    listItems += "<li>" + germanWords[i] + "</li>"
+    for (let i = 0; i < words.length; i++){
+        listItems += `<li> ${words[i]} </li>`
 }
 
 ulEl.innerHTML = listItems
 }
+
+delbtn.addEventListener("click", function() {
+    localStorage.clear()
+    germanWords = []
+    renderWords(germanWords)
+})
+
+inputBtn.addEventListener("click", function(){
+    germanWords.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("germanWords", JSON.stringify(germanWords))
+    renderWords(germanWords)
+})
