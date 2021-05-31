a1979f023a2bf21c7080b82eb6185798d5081a08
MorganBare <morganjbare@gmail.com>
2021-05-31 13:17:50 +0200

    Extension can now translate German to English words and display them

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 let germanWords = []
-let storedGermanWords = []
+let englishWords = []
 const inputBtn = document.getElementById("input-btn")
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
+const ulEL2 = document.getElementById("ul-el2")
 const delbtn = document.getElementById("del-btn")
 const germanWordsFromLocalStorage = JSON.parse( localStorage.getItem("germanWords"))
+const englishWordsFromLocalStorage = JSON.parse(localStorage.getItem("englishWords"))
 
-if (germanWordsFromLocalStorage) {
+if (germanWordsFromLocalStorage && englishWordsFromLocalStorage) {
     germanWords = germanWordsFromLocalStorage
-    renderWords(germanWords)
+    englishWords = englishWordsFromLocalStorage
+    renderGermanWords(germanWords)
+    renderEnglishWords(englishWords)
 }
 
-function renderWords(words) {
+function renderGermanWords(words) {
     let listItems = ""
     for (let i = 0; i < words.length; i++){
         listItems += `<li> ${words[i]} </li>`
@@ -20,15 +24,41 @@ function renderWords(words) {
 ulEl.innerHTML = listItems
 }
 
+function renderEnglishWords(words) {
+    let listItems = ""
+    for (let i = 0; i < words.length; i++){
+        listItems += `<li> ${words[i]} </li>`
+}
+
+ulEL2.innerHTML = listItems
+}
+
+function stringToList(word) {
+    englishWords.push(word)
+    localStorage.setItem("englishWords", JSON.stringify(englishWords))
+    renderEnglishWords(englishWords)
+}
+
 delbtn.addEventListener("click", function() {
     localStorage.clear()
     germanWords = []
-    renderWords(germanWords)
+    englishWords = []
+    renderGermanWords(germanWords)
+    renderEnglishWords(englishWords)
 })
 
 inputBtn.addEventListener("click", function(){
     germanWords.push(inputEl.value)
+    translateToEnglish(inputEl.value)
     inputEl.value = ""
     localStorage.setItem("germanWords", JSON.stringify(germanWords))
-    renderWords(germanWords)
+    renderGermanWords(germanWords)
 })
+
+// translate German word to english using DeepL API
+function translateToEnglish(word) {
+    fetch(`https://api-free.deepl.com/v2/translate?auth_key=API_KEY_GOES_HERE&text=${word}&source_lang=DE&target_lang=EN`)
+      .then((response) => response.json())
+      .then((data) => stringToList(data.translations[0].text))
+  }
+
